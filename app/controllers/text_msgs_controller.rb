2afe415eed0d743f6024ce9c1b7967e6b9dd5396
Jim Knight <jim.knight@lavatech.com>
2012-03-16 19:37:57 -0400

    body only

--- app/controllers/text_msgs_controller.rb
+++ app/controllers/text_msgs_controller.rb
@@ -12,8 +12,7 @@ class TextMsgsController < ApplicationController
     if params[:text_msg]
       @text_msg = TextMsg.new(params[:text_msg])
     else
-      binding.pry
-      @text_msg = TextMsg.new(params)
+      @text_msg = TextMsg.new(:Body => params[:Body])
     end
     if @text_msg.save
       render :text => "<Response><Sms>test</Sms></Response>"
