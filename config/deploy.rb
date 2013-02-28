cebc229b9d353696d2b50848e020ab1a33fdca14
Jim Knight <jim.knight@lavatech.com>
2013-02-28 18:58:18 -0500

    retry after installing whenever and bundler on the server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 require "bundler/capistrano"
-# set :whenever_command, "bundle exec whenever"
+set :whenever_command, "bundle exec whenever"
 require "whenever/capistrano"
 
 load "config/recipes/base"
